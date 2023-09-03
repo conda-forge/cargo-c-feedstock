f7a5f8e392840270b013991bdddb5f4bb9fe47a4
Daniel Ching <carterbox@users.noreply.github.com>
2023-09-03 12:20:15 -0500

    Include lib sub directory

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ export PKG_CONFIG_SYSROOT_DIR=$PREFIX
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
 # Help the linker can find zlib
-export LIBRARY_PATH=$PREFIX${LIBRARY_PATH:+:$LIBRARY_PATH}
+export LIBRARY_PATH=$PREFIX/lib${LIBRARY_PATH:+:$LIBRARY_PATH}
 
 cargo install --path . --root ${PREFIX}
 
