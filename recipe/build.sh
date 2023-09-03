abe976eec82cf08acdae730ac8cf0cdc7eccf41e
Daniel Ching <carterbox@users.noreply.github.com>
2023-09-03 12:17:15 -0500

    Help the linker find libz

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,11 @@
 set -ex
 
-# Required for cross-compiling
+# Required for cross-compiling with pkg-config
 export PKG_CONFIG_SYSROOT_DIR=$PREFIX
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+
+# Help the linker can find zlib
+export LIBRARY_PATH=$PREFIX${LIBRARY_PATH:+:$LIBRARY_PATH}
 
 cargo install --path . --root ${PREFIX}
 
