abc2d2ff807df182eb7471e52d00c42766b712ba
Daniel Ching <carterbox@users.noreply.github.com>
2023-09-03 11:51:18 -0500

    BUG: Add pkgconfig paths for cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 set -ex
 
+# Required for cross-compiling
+export PKG_CONFIG_SYSROOT_DIR=$PREFIX
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+
 cargo install --path . --root ${PREFIX}
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
