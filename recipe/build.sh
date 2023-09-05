7ecc7ababd46d9161fe8571b78123c09ac6f6359
Mark Harfouche <mark.harfouche@gmail.com>
2023-09-04 23:16:31 -0400

    Try to add pkgcon

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,17 @@ set -ex
 export PKG_CONFIG_SYSROOT_DIR=$PREFIX
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
-# Help the linker can find zlib
-export LIBRARY_PATH=$PREFIX/lib${LIBRARY_PATH:+:$LIBRARY_PATH}
+CARGO_TARGET="${HOST}"
+CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/conda/unknown/")"
+CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/darwin.*/darwin/")"
+CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/arm64/aarch64/")"
 
-cargo install --path . --root ${PREFIX}
+# Help the linker can find zlib
+# export LIBRARY_PATH=$PREFIX/lib${LIBRARY_PATH:+:$LIBRARY_PATH}
+
+cargo install \
+    --path . \
+    --root "${PREFIX}" \
+    --target "${CARGO_TARGET}"
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
