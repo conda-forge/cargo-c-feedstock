4d9f13f6843b4b1eeccf149b7ee59063bc9acbff
Daniel Ching <carterbox@users.noreply.github.com>
2023-09-06 11:37:29 -0500

    Add PREFIX to rustc link path

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,39 +3,15 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Required for cross-compiling with pkg-config
 export PKG_CONFIG_SYSROOT_DIR=$PREFIX
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
-
-# Help the linker can find zlib
-export LIBRARY_PATH=$PREFIX/lib${LIBRARY_PATH:+:$LIBRARY_PATH}
-
-export CARGO_HOME="$BUILD_PREFIX/cargo"
-mkdir $CARGO_HOME
+export CARGO_BUILD_RUSTFLAGS="$CARGO_BUILD_RUSTFLAGS -L all=$PREFIX/lib"
 
 if [[ "$c_compiler" == "clang" ]]; then
   echo "-L$BUILD_PREFIX/lib -Wl,-rpath,$BUILD_PREFIX/lib" > $BUILD_PREFIX/bin/$BUILD.cfg
   echo "-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib" > $BUILD_PREFIX/bin/$HOST.cfg
 fi
 
-(
-  # Needed to bootstrap itself into the conda ecosystem
-  unset CARGO_BUILD_TARGET
-  export CARGO_BUILD_RUSTFLAGS=$(echo $CARGO_BUILD_RUSTFLAGS | sed "s@$PREFIX@$BUILD_PREFIX@g")
-  export RUSTFLAGS=$CARGO_BUILD_RUSTFLAGS
-  export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
-  unset MACOSX_DEPLOYMENT_TARGET
-  unset CFLAGS
-  unset CPPFLAGS
-  unset LDFLAGS
-  unset PREFIX
-  # Check that all downstream libraries licenses are present
-  export PATH=$CARGO_HOME/bin:$PATH
-)
-
-echo $CONDA_RUST_HOST, $CONDA_RUST_TARGET
-
-export RUSTFLAGS=$CARGO_BUILD_RUSTFLAGS
-
 # build statically linked binary with Rust
-cargo install --path . --root ${PREFIX} --verbose --locked 
+cargo install --path . --root ${PREFIX} --verbose --locked
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
