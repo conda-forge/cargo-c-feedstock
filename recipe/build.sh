c3664a355e235d7634aa5e5749fcceef47ffdcd8
Daniel Ching <carterbox@users.noreply.github.com>
2023-09-08 10:40:29 -0500

    BUG: Add pkgconfig paths for cross-compile (#1)
    Add binary stripping and add PREFIX to the library linking path for cross compiling
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,25 @@
-set -ex
+set -o xtrace -o nounset -o pipefail -o errexit
 
-cargo install --path . --root ${PREFIX}
+# Required for cross-compiling with pkg-config
+export PKG_CONFIG_SYSROOT_DIR=$PREFIX
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+export CARGO_BUILD_RUSTFLAGS="$CARGO_BUILD_RUSTFLAGS -L all=$PREFIX/lib"
+
+if [[ "$c_compiler" == "clang" ]]; then
+  echo "-L$BUILD_PREFIX/lib -Wl,-rpath,$BUILD_PREFIX/lib" > $BUILD_PREFIX/bin/$BUILD.cfg
+  echo "-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib" > $BUILD_PREFIX/bin/$HOST.cfg
+fi
+
+# build statically linked binary with Rust
+cargo install --path . --root ${PREFIX} --verbose --locked
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+# strip debug symbols
+"$STRIP" "$PREFIX/bin/cargo-cinstall"
+"$STRIP" "$PREFIX/bin/cargo-cbuild"
+"$STRIP" "$PREFIX/bin/cargo-ctest"
+"$STRIP" "$PREFIX/bin/cargo-capi"
+
+# remove extra build file
+rm -f "${PREFIX}/.crates.toml"
